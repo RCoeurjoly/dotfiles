f75dad79bab20b83a454ddecbe70044f40fb6d96
rcl <rolandcoeurjoly@gmail.com>
2019-05-12 14:30:08 +0200

    Fix stow

--- install.sh
+++ install.sh
@@ -3,16 +3,15 @@
 # Install all the necessary Debian packages, especially `stow`.
 yes | ./debian_packages_installation.sh
 
-stow ~/dotfiles/bash
-stow ~/dotfiles/spacemacs
-stow ~/dotfiles/email
-stow ~/dotfiles/firefox
-stow ~/dotfiles/git
-stow ~/dotfiles/lisp
-stow ~/dotfiles/rss
-stow ~/dotfiles/ruby
-stow ~/dotfiles/x-windows
-
 # Link .bash_profile -> .bashrc
-rm -f ~/.bash_profile
-ln -s ~/.bashrc ~/.bash_profile
+rm -f ~/.bashrc
+
+stow bash
+stow spacemacs
+stow email
+stow firefox
+stow git
+stow lisp
+stow rss
+stow ruby
+stow x-windows
