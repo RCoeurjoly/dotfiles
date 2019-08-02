6811fbea0464652e24a2f2f075042e1da8076ced
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2019-08-02 13:00:51 +0200

    Append to history inmediately after command

--- bash/.bashrc.d/utils.bash
+++ bash/.bashrc.d/utils.bash
@@ -154,3 +154,9 @@ if ! shopt -oq posix; then
     . /etc/bash_completion
   fi
 fi
+
+# When the shell exits, append to the history file instead of overwriting it
+shopt -s histappend
+
+# After each command, append to the history file and reread it
+export PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r"
