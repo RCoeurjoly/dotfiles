40bae3ca713716e1450a358e6b0c5b3092e2e7fe
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2019-10-09 23:41:55 +0200

    Refactor

--- debian_packages_installation.sh
+++ debian_packages_installation.sh
@@ -1,10 +1,73 @@
 #!/bin/sh
 
+# Installing Debian packages
+
+apt-get install \
+        abook \
+        aptitude \
+        arandr \
+        build-essential \
+        compton \
+        curl \
+        feh \
+        git \
+        gksu \
+        gnome-terminal \
+        gnupg2 \
+        gnuplot \
+        graphviz \
+        htop \
+        i3 \
+        imagemagick \
+        isync \
+        libnotify-bin \
+        msmtp \
+        mutt \
+        notmuch \
+        notmuch-mutt \
+        openssl \
+        pandoc \
+        pass \
+        pasystray \
+        pcmanfm \
+        procmail \
+        pwgen \
+        redshift \
+        rlwrap \
+        rofi \
+        rxvt-unicode \
+        scrot \
+        silversearcher-ag \
+        stow \
+        sudo \
+        texlive-base \
+        texlive-fonts-recommended \
+        texlive-latex-base \
+        tree \
+        unclutter \
+        unifont \
+        unar \
+        urlscan \
+        urlview \
+        vim \
+        w3m \
+        xbindkeys \
+        xcape \
+        xdotool \
+        xsel \
+        zathura
+
+# For tlp power management.
+apt-get install \
+        acpi-call-dkms \
+        tlp \
+        tp-smapi-dkms
+
 # Installing emacs26
 
-sudo add-apt-repository ppa:kelleyk/emacs
-sudo apt-get update
-sudo apt-get install emacs26
+add-apt-repository ppa:kelleyk/emacs
+apt-get update
+apt-get install emacs26
 
 # Installing Spacemacs
 
@@ -13,66 +76,3 @@ git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 # Cloning Exocortex
 
 git clone https://github.com/RCoeurjoly/Exocortex ~/Exocortex
-
-# Installing Debian packages
-
-apt-get install stow \
-abook \
-aptitude \
-arandr \
-build-essential \
-compton \
-curl \
-feh \
-git \
-gksu \
-gnome-terminal \
-gnupg2 \
-gnuplot \
-graphviz \
-htop \
-i3 \
-imagemagick \
-isync \
-libnotify-bin \
-msmtp \
-mutt \
-notmuch \
-notmuch-mutt \
-openssl \
-pandoc \
-pass \
-pasystray \
-pcmanfm \
-procmail \
-pwgen \
-redshift \
-rlwrap \
-rofi \
-rxvt-unicode \
-scrot \
-silversearcher-ag \
-stow \
-sudo \
-texlive-base \
-texlive-fonts-recommended \
-texlive-latex-base \
-tree \
-unclutter \
-unifont \
-unar \
-urlscan \
-urlview \
-vim \
-w3m \
-xbindkeys \
-xcape \
-xdotool \
-xsel \
-zathura
-
-# For tlp power management.
-sudo apt-get install \
-     acpi-call-dkms \
-     tlp \
-     tp-smapi-dkms
