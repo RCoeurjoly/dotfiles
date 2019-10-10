e4c986bc669086642ae7f5ed7cefc95f704b0133
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2019-10-10 12:15:44 +0200

    Fix spacing for apt packages

--- debian_packages_installation.sh
+++ debian_packages_installation.sh
@@ -3,59 +3,59 @@
 # Installing Debian packages
 
 sudo apt-get install \
-        abook \
-        aptitude \
-        arandr \
-        build-essential \
-        compton \
-        curl \
-        feh \
-        git \
-        gksu \
-        gnome-terminal \
-        gnupg2 \
-        gnuplot \
-        graphviz \
-        htop \
-        i3 \
-        imagemagick \
-        isync \
-        libnotify-bin \
-        msmtp \
-        mutt \
-        notmuch \
-        notmuch-mutt \
-        openssl \
-        pandoc \
-        pass \
-        pasystray \
-        pcmanfm \
-        procmail \
-        pwgen \
-        redshift \
-        rlwrap \
-        rofi \
-        rxvt-unicode \
-        scrot \
-        silversearcher-ag \
-        stow \
-        sudo \
-        texlive-base \
-        texlive-fonts-recommended \
-        texlive-latex-base \
-        tree \
-        unclutter \
-        unifont \
-        unar \
-        urlscan \
-        urlview \
-        vim \
-        w3m \
-        xbindkeys \
-        xcape \
-        xdotool \
-        xsel \
-        zathura
+abook \
+aptitude \
+arandr \
+build-essential \
+compton \
+curl \
+feh \
+git \
+gksu \
+gnome-terminal \
+gnupg2 \
+gnuplot \
+graphviz \
+htop \
+i3 \
+imagemagick \
+isync \
+libnotify-bin \
+msmtp \
+mutt \
+notmuch \
+notmuch-mutt \
+openssl \
+pandoc \
+pass \
+pasystray \
+pcmanfm \
+procmail \
+pwgen \
+redshift \
+rlwrap \
+rofi \
+rxvt-unicode \
+scrot \
+silversearcher-ag \
+stow \
+sudo \
+texlive-base \
+texlive-fonts-recommended \
+texlive-latex-base \
+tree \
+unclutter \
+unifont \
+unar \
+urlscan \
+urlview \
+vim \
+w3m \
+xbindkeys \
+xcape \
+xdotool \
+xsel \
+zathura
 
 # For tlp power management.
 sudo apt-get install \
