f3cee8637c4ad47eb1b177042da837ac6e37fc90
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2020-02-09 16:39:35 +0100

    Remove unused packages

--- install_debian_packages.sh
+++ install_debian_packages.sh
@@ -2,16 +2,12 @@
 
 # Installing Debian packages
 
-DEBIAN_PACKAGES="abook \
-     aptitude \
+DEBIAN_PACKAGES="aptitude \
      arandr \
      build-essential \
-     compton \
      curl \
-     dirmngr \
      docker \
      docker-compose \
-     feh \
      gdb \
      gdbserver \
      git \
@@ -19,46 +15,16 @@ DEBIAN_PACKAGES="abook \
      gnupg2 \
      gnuplot \
      graphviz \
-     htop \
      i3 \
      imagemagick \
-     isync \
-     libnotify-bin \
-     msmtp \
-     mutt \
-     notmuch \
-     notmuch-mutt \
      openssl \
      pandoc \
-     pass \
-     pasystray \
-     pcmanfm \
-     procmail \
-     pwgen \
-     redshift \
-     rlwrap \
-     rofi \
-     rxvt-unicode \
-     scrot \
-     silversearcher-ag \
      stow \
      sudo \
      texlive-base \
      texlive-fonts-recommended \
      texlive-latex-base \
-     tree \
-     unclutter \
-     unifont \
      unar \
-     urlscan \
-     urlview \
-     vim \
-     w3m \
-     xbindkeys \
-     xcape \
-     xdotool \
-     xsel \
-     zathura
 "
 
 for package in ${DEBIAN_PACKAGES}; do
