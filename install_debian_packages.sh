ada4966a0e38a8585f2328059b29da09c476ff9b
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2020-02-09 14:28:36 +0100

    Add bash installation

--- install_debian_packages.sh
+++ install_debian_packages.sh
@@ -80,3 +80,7 @@ git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 # Installing Cask
 
 curl -fsSL https://raw.githubusercontent.com/cask/cask/master/go | python
+
+# Install bash it
+
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
