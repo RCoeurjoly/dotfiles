aef25a82fcafa83a2ed52b14fddd11b86ee26678
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2020-02-12 09:25:40 +0100

    Piezas scripts

--- scripts/startall.sh
+++ scripts/startall.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-sudo /home/drcoeurjoly/DF5555xx/script/DF5555xx start
-/home/drcoeurjoly/MD5555xx/script/MD5555xx start
-/home/drcoeurjoly/SS5555xx/script/SS5555xx start
-sudo /data/programs/oms/RT5555xx/script/RT5555xx start
+sudo ln -fs /home/drcoeurjoly/DF5555xx/ /data/programs/oms/DF5555xx/
+sudo ln -fs /home/drcoeurjoly/MD5555xx/ /data/programs/oms/MD5555xx/
+sudo ln -fs /home/drcoeurjoly/SS5555xx/ /data/programs/oms/SS5555xx/
+sudo ln -fs /home/drcoeurjoly/RT5555xx/ /data/programs/oms/RT5555xx/
+
+sudo /home/drcoeurjoly/DF5555xx/bin/DF5555xx /home/drcoeurjoly/DF5555xx/etc/DF5555xx.cfg &
+/home/drcoeurjoly/MD5555xx/bin/MD5555xx /home/drcoeurjoly/MD5555xx/etc/MD5555xx.cfg &
+/home/drcoeurjoly/SS5555xx/bin/SS5555xx /home/drcoeurjoly/SS5555xx/etc/SS5555xx.cfg &
+sudo /home/drcoeurjoly/RT5555xx/bin/RT5555xx /home/drcoeurjoly/RT5555xx/etc/RT5555xx.cfg &
