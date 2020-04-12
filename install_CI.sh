23a2f1c56ffe6574e95846898611fc4204216c54
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2020-04-13 00:21:40 +0200

    WIP

--- install_CI.sh
+++ install_CI.sh
@@ -1,35 +1,3 @@
-INITIALDIR=$( pwd )
-git clone https://github.com/phatina/simple-mtpfs ~/simple-mtpfs
-cd ~/simple-mtpfs
-./autogen.sh
-mkdir build && cd build
-../configure
-make
-sudo make install
-rm -rf ~/simple-mtpfs
-cd $INITIALDIR
-
-git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-
-
-
-git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-
-curl https://nixos.org/nix/install | sh
-. /home/runner/.nix-profile/etc/profile.d/nix.sh
-
-nix-env --install emacs
-for location in $(whereis emacs);
-do
-    SEARCH_RESULT="$(echo $location | grep "\/nix.*user-environment\/bin\/emacs")"
-    if [ "${SEARCH_RESULT}" ]; then
-        NIX_EMACS=${SEARCH_RESULT}
-        sudo rm /usr/bin/emacs
-        sudo ln -s ${NIX_EMACS} /usr/bin/emacs;
-    fi
-done
-
-curl -fsSL https://raw.githubusercontent.com/cask/cask/master/go | python
 DEBIAN_PACKAGES="aptitude \
                         arandr \
                         build-essential \
@@ -73,6 +41,40 @@ do
     echo
     sudo apt -y install $package >&/dev/null
 done
+
+INITIALDIR=$( pwd )
+git clone https://github.com/phatina/simple-mtpfs ~/simple-mtpfs
+cd ~/simple-mtpfs
+./autogen.sh
+mkdir build && cd build
+../configure
+make
+sudo make install
+rm -rf ~/simple-mtpfs
+cd $INITIALDIR
+
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+
+
+
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+curl https://nixos.org/nix/install | sh
+. /home/runner/.nix-profile/etc/profile.d/nix.sh
+
+nix-env --install emacs
+for location in $(whereis emacs);
+do
+    SEARCH_RESULT="$(echo $location | grep "\/nix.*user-environment\/bin\/emacs")"
+    if [ "${SEARCH_RESULT}" ]; then
+        NIX_EMACS=${SEARCH_RESULT}
+        sudo rm /usr/bin/emacs
+        sudo ln -s ${NIX_EMACS} /usr/bin/emacs;
+    fi
+done
+
+curl -fsSL https://raw.githubusercontent.com/cask/cask/master/go | python
+
 stow bash
 stow spacemacs
 stow x-windows
