e6dd121db6f5502aaca52fe17ad7205dc87ff223
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2020-10-02 07:26:38 +0000

    WIP

---
+++ scripts.py
@@ -0,0 +1,17 @@
+import sys
+import re
+from os.path import expanduser
+
+JIRA_PATH_CORRESPONDENCE = {"DRVINET":    expanduser("~") + "/driiiiiiiiiiiiiiiiiiiivers/inetdriver/",
+                            "OMSRBT":     expanduser("~") + "/oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooms/oms-trade-robot",
+                            "VTGTWY":     expanduser("~") + "/inntech/desarrollo/gateway/vtgateway",
+                            "VTFIXBRDG":  expanduser("~") + "/inntech/desarrollo/gateway/fixbridge"}
+
+def remove_code(JIRA):
+    return re.sub(r'-[0-9]*', '', JIRA)
+
+def find_project_path_with_JIRA(JIRA):
+    return JIRA_PATH_CORRESPONDENCE[remove_code(JIRA)]
+
+if __name__ == "__main__":
+    sys.stdout.write("%s\n" % (find_project_path_with_JIRA(sys.argv[1])))
